f4477474e7281144540009e2d9ebfef6f7bb7796
MrKravz <4otkiipozzik44@gmail.com>
2026-08-19 16:42:31 +0300

    init commit

---
+++ src/main/java/by/ares/observ_ex/ObservExApplication.java
@@ -0,0 +1,13 @@
+package by.ares.observ_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/ares/observ_ex/ObservExApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ares.observ_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
